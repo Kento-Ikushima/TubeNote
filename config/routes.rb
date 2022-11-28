5c85ad1a97a2d3f3d9ff591b878dfb23be23cffb
Kento-Ikushima <114498754+Kento-Ikushima@users.noreply.github.com>
2022-11-28 09:19:45 +0900

    devise導入
    deviseを導入、名前とパスワードでログインできるようにしたかったので、そのようにしています。

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'tasks/index'
+  
+  root to: 'tasks#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
