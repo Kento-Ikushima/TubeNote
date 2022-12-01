13f5b6e875d5f21444255fbc5474c4741dd14742
Kento-Ikushima <114498754+Kento-Ikushima@users.noreply.github.com>
2022-12-01 15:31:07 +0900

    アカウント編集画面にうつるボタン作成、編集画面にコメント記入欄設置
    タスク一覧画面から、アカウント編集画面にうつるようにしました。
    アカウント編集画面にコメントの編集箇所を追加しました。
    コントローラーで:emailと:self_introdoctionのカラムを許可したつもりなのですが、動かしてみると、その２つのカラムは通りませんでした。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
-    before_action :configure_permitted_parameters, if: :devise_controller? 
+  before_action :configure_permitted_parameters, if: :devise_controller? 
 
   private
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up,keys:[:email]) # 注目
+      devise_parameter_sanitizer.permit(:sign_up,keys:[:email, :self_introduction]) # 注目
     end
     
     end
