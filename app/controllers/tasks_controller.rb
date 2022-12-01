c93e1bfb21af44ed3e995abbe4e4e02ad3c511b2
Kento-Ikushima <114498754+Kento-Ikushima@users.noreply.github.com>
2022-12-01 10:05:52 +0900

    createアクション指摘箇所の訂正
    本来は消すべきだと分かったうえで、今回はこのブランチで変更を加えます。
    
    11桁うんぬんは一度放置しました。
    メソッドを定義。削除カラムの追加は、別のブランチで行います。

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,8 +5,16 @@ class TasksController < ApplicationController
 
   def create
     @task = Task.new(task_params)
-    url = params[:task][:url]
-    url = url.last(11)
-    @task.url = url
+    # url = params[:task][:url]
+    # url = url.last(11)
+    # @task.url = url
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def task_params
+    params.require(:task).permit(:url, :description, :user_id, :folder_id)
   end
 end
