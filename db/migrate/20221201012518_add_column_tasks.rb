bbbea940d279fe084a15e357bfdac890466bbf50
Kento-Ikushima <114498754+Kento-Ikushima@users.noreply.github.com>
2022-12-01 10:32:12 +0900

    タスクテーブルに削除カラム追加
    指摘いただいていた、削除カラムを追加しました。

---
+++ db/migrate/20221201012518_add_column_tasks.rb
@@ -0,0 +1,5 @@
+class AddColumnTasks < ActiveRecord::Migration[7.0]
+  def change
+    add_column :tasks, :delete, :integer, null: false, default: 0, comment: "削除は1"
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_27_235041) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_01_012518) do
   create_table "folders", force: :cascade do |t|
     t.string "folder_name", null: false
     t.integer "status", null: false
@@ -40,6 +40,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_27_235041) do
     t.integer "folder_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "delete", default: 0, null: false
   end
 
   create_table "users", force: :cascade do |t|
