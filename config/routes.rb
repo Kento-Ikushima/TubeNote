caf5a34f57964f170480d70d86e88cda054150c4
Kento-Ikushima <114498754+Kento-Ikushima@users.noreply.github.com>
2022-12-02 18:27:13 +0900

    deviseの導入を続きから行いました
    コントローラーの作成
    ルートへの記述
    を行いました。

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions'   
+  } 
+
+  devise_scope :user do
+    get "user/:id", :to => "users/registrations#detail"
+    get "signup", :to => "users/registrations#new"
+    get "login", :to => "users/sessions#new"
+    get "logout", :to => "users/sessions#destroy"
+  end
+  
   resources :users
   resources :tasks
   resources :followings
