31ab2fae385262f14583a4b5ac06c21fe70cd86e
Kento-Ikushima <114498754+Kento-Ikushima@users.noreply.github.com>
2022-12-05 23:13:35 +0900

    指摘箇所を直しました
    不要なコードを削除しました。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,7 @@ class ApplicationController < ActionController::Base
     
   
     private
-
-    def set_csrf_token_header
-      response.set_header('X-CSRF-Token', form_authenticity_token)
-    end
-
+    
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys:[:email, :self_introduction, :name]) 
       devise_parameter_sanitizer.permit(:account_update, keys:[:email, :self_introduction, :name])
