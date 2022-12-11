405181244ebe43d059a46982258215dde8da951b
Kento-Ikushima <114498754+Kento-Ikushima@users.noreply.github.com>
2022-12-11 11:54:40 +0900

    フォルダの一覧画面および編集画面の作成
    フォルダに関して、
    　一覧画面の作成
    　編集画面の作成
    を行いました。

--- app/controllers/folders_controller.rb
+++ app/controllers/folders_controller.rb
@@ -1,17 +1,30 @@
 class FoldersController < ApplicationController
-    def index
-    end
+  def index
+    @folders = current_user.folders
+  end
     
-    def create
-    end
+  def create
+  end
     
-    def new
-    end
+  def new
+  end
 
-    def edit
-    end
+  def edit
+    @folder = Folder.find(params[:id])
+  end
     
-    def delete
-    end
+  def update
+    folder = Folder.find(params[:id])
+    folder.update!(folder_params)
+    redirect_to folders_url
+  end
+      
+  def delete
+  end
     
+  private
+  def folder_params
+    params.require(:folder).permit(:folder_name, :status)
+  end
+
 end
