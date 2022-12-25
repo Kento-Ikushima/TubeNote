afc738aca9699318c163d6fe28919b6fe0cae132
Kento-Ikushima <114498754+Kento-Ikushima@users.noreply.github.com>
2022-12-25 14:18:16 +0900

    フォルダまわりの実装
    スプレッドシートNo16「フォルダの一括削除」に関するPRです。
    
    ①一括削除実装、②個々の削除の実装、③新規作成ビューの作成、の３つのことを行っています。

--- app/controllers/folders_controller.rb
+++ app/controllers/folders_controller.rb
@@ -4,9 +4,13 @@ class FoldersController < ApplicationController
   end
     
   def create
+    @folder = current_user.folders.new(folder_params)
+    @folder.save
+    redirect_to folders_path
   end
     
   def new
+    @folder = Folder.new
   end
 
   def edit
@@ -19,9 +23,21 @@ class FoldersController < ApplicationController
     redirect_to folders_url
   end
       
-  def delete
+  def destroy
+    @folder = Folder.find(params[:id])
+    @folder.destroy
+    redirect_to folders_path
   end
-    
+
+  def destroy_many
+    checked_date = params[:deletes].keys
+    if Folder.destroy(checked_date)
+      redirect_to folders_path
+    else
+      render action: 'index'
+    end
+  end
+
   private
   def folder_params
     params.require(:folder).permit(:folder_name, :status)
