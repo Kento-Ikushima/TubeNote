b11686043468494415829b8409864d1e7f614f0a
Kento-Ikushima <114498754+Kento-Ikushima@users.noreply.github.com>
2023-01-04 12:37:13 +0900

    指摘箇所の修正

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,3 @@
-//= link_tree ../images
 //= link_directory ../stylesheets .css
+//= link_directory ../stylesheets .scss
 //= link_directory ../javascripts .js
-//= link my_tasks/my_tasks.scss.css
-//= link sessions/sessions.scss.css
