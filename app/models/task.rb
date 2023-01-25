667a836848735c5a482d46eaf7a55c37f9ae7cdd
Kento-Ikushima <114498754+Kento-Ikushima@users.noreply.github.com>
2023-01-25 19:23:35 +0900

    taskモデルに対するバリデーションの追加
    タスクに関する「空」をはじくため、バリデーションを記述しました。

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,7 @@
 class Task < ApplicationRecord
   belongs_to :user
+  validates :user_id, presence: true
+  validates :url, presence: true
+  validates :description, presence: true
+  validates :folder_id, presence: true
 end
