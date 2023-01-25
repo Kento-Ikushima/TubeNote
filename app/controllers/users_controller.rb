37f62ee2f56cd3970c35f6d9f3ff70f34aebe730
Kento-Ikushima <114498754+Kento-Ikushima@users.noreply.github.com>
2023-01-25 11:31:09 +0900

    指摘箇所の修正
    インスタンス変数@following_userと@follower_userに代入する形で、以前html.erbに記述していたコードを、コントローラーに記述しました。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,10 @@
 class UsersController < ApplicationController
   def show
     @other_user = User.find(params[:id])
-    release = Folder.select(:id).where(status:0)
-    @tasks = Task.where(folder_id:release).where(user_id:params[:id])
+    release = Folder.select(:id).where(status: 0)
+    @tasks = Task.where(folder_id: release).where(user_id: params[:id])
+    @following_user = @other_user.following_user.where.not(id: current_user.id)
+    @follower_user = @other_user.follower_user.where.not(id: current_user.id)
   end
 
   
